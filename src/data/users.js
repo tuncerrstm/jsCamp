5c482043a9fe75a70a9ebed8a25a6df2969e2f67
tuncerrstm <tuncerrstm@gmail.com>
2021-11-25 00:19:13 +0300

    ADD: authorization processes

---
+++ src/data/users.js
@@ -0,0 +1,47 @@
+export const users = [
+    {
+        id:1,
+        firstName: "Tuncer",
+        lastName: "Rüstemoğlu",
+        age: 30,
+        creditCardNumber:"123456",
+        type: "customer"
+    },
+    {
+        id:2,
+        firstName: "Muazes",
+        lastName: "Rüstemoğlu",
+        city: "İstanbul",
+        age: "sagadg29a",
+        creditCardNumber:"654321",
+        type: "customer"
+    },
+    {
+        id:3,
+        firstName: "Hakan",
+        lastName: "Ali",
+        city: "Kırcaali",
+
+        salary: 7000,
+        type: "employee"
+    },
+    {
+        id:4,
+        firstName: "Tolga",
+        lastName: "Değirmen",
+        city: "Ankara",
+        age: 21,
+        type: "employee"
+    },
+    {
+        id:5,
+        firstName: "Eren",
+        lastName: "Arı",
+        city: "Kayseri",
+        age: 25,
+        salary:15000,
+        type: "employe"
+    },
+
+
+]
